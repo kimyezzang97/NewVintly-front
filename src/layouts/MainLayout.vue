9e2335f2acf6ce9884c6a139248738c775c5ea6e
kimyezzang97 <kimyezzang97@gmail.com>
2025-03-17 15:43:57 +0900

    feat: prettier 설정

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,17 +1,23 @@
 <template>
-   <v-layout class="rounded rounded-md">
+  <v-layout class="rounded rounded-md">
     <v-app-bar color="surface-variant" title="">
       <!-- d-flex : Flexbox 활성화, justify-center : 수평 가운데, align-center : 수직 가운데 -->
-      <v-container class="d-flex justify-center"> 
-        <RouterLink to="/" tag="div">
-          <v-img src="@/assets/logo_brown.png" width="130px" class="cursor-pointer"/>      
+      <v-container class="d-flex justify-center">
+        <RouterLink to="/">
+          <v-img
+            src="@/assets/logo_brown.png"
+            width="130px"
+            class="cursor-pointer"
+          />
+          <!-- 해상도는 1440으로 하자 -->
         </RouterLink>
       </v-container>
     </v-app-bar>
 
-   
-
-    <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
+    <v-main
+      class="d-flex align-center justify-center"
+      style="min-height: 300px"
+    >
       Main Content
     </v-main>
   </v-layout>
@@ -19,20 +25,19 @@
 
 <script setup lang="ts">
 defineOptions({
-  name: 'MainLayout',
+  name: "MainLayout",
 });
 
-const linksList = [
-  {
-    title: 'Login',
-    to: '/login',
-  },
-  {
-    title: 'Join',
-    to: '/join',
-  },
-];
+// const linksList = [
+//   {
+//     title: 'Login',
+//     to: '/login',
+//   },
+//   {
+//     title: 'Join',
+//     to: '/join',
+//   },
+// ];
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,25 @@
-const { defineConfig } = require('@vue/cli-service');
-const ESLintPlugin = require('eslint-webpack-plugin');
+const { defineConfig } = require("@vue/cli-service");
+const ESLintPlugin = require("eslint-webpack-plugin");
+
+//import { defineConfig } from '@vue/cli-service';
+//import ESLintPlugin from 'eslint-webpack-plugin';
 
 module.exports = defineConfig({
   transpileDependencies: true,
 
   pluginOptions: {
     vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+    },
   },
-   // Webpack 설정 수정
-   chainWebpack: config => {
+  // Webpack 설정 수정
+  chainWebpack: (config) => {
     // 기존 eslint-loader를 eslint-webpack-plugin으로 변경
-    config.plugin('eslint').use(ESLintPlugin, [
+    config.plugin("eslint").use(ESLintPlugin, [
       {
         emitWarning: true, // 경고 메시지를 표시할지 여부
         // extensions 옵션 제거
       },
     ]);
   },
-})
+});
