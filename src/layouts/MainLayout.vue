f8fc2289382bf2d028a53b4b5b2092320d727e88
kimyezzang97 <kimyezzang97@gmail.com>
2025-03-17 17:37:06 +0900

    feat: 메인 페이지 화면 수정

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,25 +1,69 @@
 <template>
-  <v-layout class="rounded rounded-md">
-    <v-app-bar color="surface-variant" title="">
-      <!-- d-flex : Flexbox 활성화, justify-center : 수평 가운데, align-center : 수직 가운데 -->
-      <v-container class="d-flex justify-center">
-        <RouterLink to="/">
-          <v-img
-            src="@/assets/logo_brown.png"
-            width="130px"
-            class="cursor-pointer"
-          />
-          <!-- 해상도는 1440으로 하자 -->
-        </RouterLink>
-      </v-container>
-    </v-app-bar>
+  <v-layout>
+    <v-app id="inspire">
+      <v-app-bar
+        color="surface-variant"
+        class="d-flex justify-center align-center"
+      >
+        <!-- d-flex : Flexbox 활성화, justify-center : 수평 가운데, align-center : 수직 가운데 -->
+        <v-container class="d-flex justify-center align-center">
+          <RouterLink to="/">
+            <v-img
+              src="@/assets/logo_brown.png"
+              width="130px"
+              class="cursor-pointer"
+            />
+          </RouterLink>
+        </v-container>
+      </v-app-bar>
+      <v-app-bar class="" density="compact" flat>
+        <v-spacer></v-spacer>
+        <v-tabs
+          align-tabs="center"
+          color="grey-darken-2"
+          class="d-flex"
+          hide-slider
+        >
+          <v-tab text="게시판" class="custom-tab"></v-tab>
+        </v-tabs>
+        <v-spacer></v-spacer>
+        <v-spacer></v-spacer>
+        <v-tabs
+          align-tabs="center"
+          color="grey-darken-2"
+          class="d-flex"
+          hide-slider
+        >
+          <v-tab text="로그인" class="custom-tab"></v-tab>
+          <v-tab text="회원가입" class="custom-tab"></v-tab>
+        </v-tabs>
+        <v-spacer></v-spacer>
+      </v-app-bar>
 
-    <v-main
-      class="d-flex align-center justify-center"
-      style="min-height: 300px"
-    >
-      Main Content
-    </v-main>
+      <v-main class="bg-grey-lighten-3">
+        <v-container>
+          <v-row>
+            <v-col cols="12" md="2">
+              <v-sheet min-height="268" rounded="lg">
+                <!--  -->
+              </v-sheet>
+            </v-col>
+
+            <v-col cols="12" md="8">
+              <v-sheet min-height="70vh" rounded="lg">
+                <!--  -->
+              </v-sheet>
+            </v-col>
+
+            <v-col cols="12" md="2">
+              <v-sheet min-height="268" rounded="lg">
+                <!--  -->
+              </v-sheet>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-main>
+    </v-app>
   </v-layout>
 </template>
 
@@ -40,4 +84,8 @@ defineOptions({
 // ];
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.custom-tab {
+  font-size: 16px;
+}
+</style>
