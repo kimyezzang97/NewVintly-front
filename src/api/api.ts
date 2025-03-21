6df63e5efc8e2a97db5ccc0727e0db82ae813f38
kimyezzang97 <kimyezzang97@gmail.com>
2025-03-21 15:18:33 +0900

    feat: loginAPI 구현

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,8 +2,10 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL: process.env.VUE_APP_BACKEND_API_URL,
-  withCredentials: true, // cookie나 token이 같이 전송 됨
+  //withCredentials: true, // cookie나 token이 같이 전송 됨
   headers: {
     "Content-Type": "application/json",
+    Accept: "application/json",
   },
+  withCredentials: true, // 쿠키 허용 (refresh token 자동 포함)
 });
