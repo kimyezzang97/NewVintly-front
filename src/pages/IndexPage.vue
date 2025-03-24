fac5e524aa7c3da4bf7c67f774c1ec0add9bc96b
kimyezzang97 <kimyezzang97@gmail.com>
2025-03-24 12:59:03 +0900

    style: indexPage 레이아웃 구현

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,6 +1,25 @@
 <template>
   <v-col cols="12" md="8">
-    <v-sheet min-height="70vh" rounded="lg"> Index페이지 </v-sheet>
+    <v-row class="d-flex justify-center">
+      <v-col class="d-flex justify-center">
+        <v-img
+          alt="vintly-bg-1"
+          src="../assets/vintly-bg-1.jpg"
+          max-width="1100px"
+          height="450px"
+        />
+      </v-col>
+    </v-row>
+
+    <!-- Tabs 영역 -->
+    <v-tabs class="mt-12" align-tabs="center">
+      <div class="text-h6" style="margin-left: 3%">NOTICE</div>
+      <v-spacer />
+      <div class="text-h7 my-2" style="margin-right: 3%">more</div>
+    </v-tabs>
+
+    <!-- Divider -->
+    <v-divider :thickness="4" />
   </v-col>
 </template>
 
