d61038e538ec3a068d146ab3374bc0f64a5fae89
kimyezzang97 <kimyezzang97@gmail.com>
2025-08-15 00:11:05 +0900

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,8 +2,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import routes from './route'; // route.ts에서 정의한 라우터 설정을 불러옴
 
+
+// 브라우저 환경에서 process가 없으므로 직접 문자열 사용
+const BASE_URL = '/'; 
+
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(BASE_URL),
   routes, // 이전에 정의한 routes 배열을 사용
 });
 
